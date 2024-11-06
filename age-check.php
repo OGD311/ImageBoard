8b75b6c9651b75f44caee941a0c7db87a3e62ffb
Oliver GD <114223604+OGD311@users.noreply.github.com>
2024-11-06 13:05:47 +0000

    Update age-check.php

--- age-check.php
+++ age-check.php
@@ -8,12 +8,7 @@ require_once 'config.php';
     <meta charset="UTF-8">
     <title>Home</title>
     <?php include 'core/html-parts/header-elems.php' ?>
-    <!-- <link href="https:cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="/static/css/navbar.css">
-    <link rel="stylesheet" href="/static/css/details.css">
-    <script src="/static/js/search-bar.js" type="module"></script>
-    <script src="/static/js/hide-page.js"></script>
-    <script src="/static/js/cookie.js" type="module"></script> -->
+
 
 </head>
 
