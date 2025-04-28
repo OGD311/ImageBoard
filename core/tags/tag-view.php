03713299617c7a21dacfaed13c5587c33b3b4997
OGD <114223604+OGD311@users.noreply.github.com>
2025-04-28 20:09:31 +0100

    Added category selection for tags

--- core/tags/tag-view.php
+++ core/tags/tag-view.php
@@ -5,7 +5,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/core/tags/tag-functions.php';
 $mysqli = $_DB; 
 if ($_SERVER['REQUEST_METHOD'] === "GET") {
 
-    $sql = sprintf("SELECT t.id, t.name, t.count
+    $sql = sprintf("SELECT t.id, t.name, t.category, t.count
     FROM tags t
     JOIN post_tags pt ON t.id = pt.tag_id
     WHERE pt.post_id = '%s'", htmlspecialchars($post['id']) );
@@ -22,35 +22,51 @@ if ($_SERVER['REQUEST_METHOD'] === "GET") {
 
 }
 
-if ($result) {
 
+if ($result) { 
+    $previousCategory = null;
     echo '<h4>Tags</h4>';
 
-    echo '<ul>';
     foreach ($tags as $tag) {
-        echo '<div  > <p>';
-        
-        $alias = get_alias($tag['id']) ? get_alias($tag['id']) : null;
+
+        $tag_category = strtolower($tag['category']);
+
+        if ($tag_category != $previousCategory) {
+            echo '</ul>
+            <h3 class=' . htmlspecialchars($tag_category) . '>' . htmlspecialchars(ucfirst($tag_category)) . '</h3>
+            <ul id="tags-list">';
+        }
+        $alias = get_alias($tag['id']) ?? null;
 
         if ($alias) {
-            echo '<li><span><a id="addTag" onclick="add_and_search(\'' . htmlspecialchars($alias['name']) . '\', true)">+</a> <a id="removeTag" onclick="add_and_search(\'-' . htmlspecialchars($alias['name']) . '\', false)">-</a></span> ' . str_replace('_', ' ', htmlspecialchars($alias['name'])) . ' (' . htmlspecialchars($alias['count']) . ')';
+            if (!in_array($alias['id'], array_column($tags, 'id'))) {
+                echo '<li class=' . htmlspecialchars($tag_category) .'><span><a id="addTag" onclick="add_and_search(\'' . htmlspecialchars($alias['name']) . '\', true)">+</a> 
+                    <a id="removeTag" onclick="add_and_search(\'-' . htmlspecialchars($alias['name']) . '\', false)">-</a></span> 
+                    ' . str_replace('_', ' ', htmlspecialchars($alias['name'])) . ' (' . htmlspecialchars($alias['count']) . ')';
+            }
+            
+            $tags = array_filter($tags, function($value) use ($alias) {
+                return $value['id'] != $alias['id'];
+            });
         } else {
-            echo '<li><span><a id="addTag" onclick="add_and_search(\'' . htmlspecialchars($tag['name']) . '\', true)">+</a> <a id="removeTag" onclick="add_and_search(\'-' . htmlspecialchars($tag['name']) . '\', false)">-</a></span> ' .  str_replace('_', ' ', htmlspecialchars($tag['name'])) . ' (' . htmlspecialchars($tag['count']) . ')';
+            echo '<li class=' . htmlspecialchars($tag_category) .'><span><a id="addTag" onclick="add_and_search(\'' . htmlspecialchars($tag['name']) . '\', true)">+</a> 
+                <a id="removeTag" onclick="add_and_search(\'-' . htmlspecialchars($tag['name']) . '\', false)">-</a></span> 
+                ' . str_replace('_', ' ', htmlspecialchars($tag['name'])) . ' (' . htmlspecialchars($tag['count']) . ')';
         }
-        
-        echo '<li>';
-        echo '</p></div>';
+
+        echo '</li>';
+        $previousCategory = $tag_category;
     }
 
-
     if ((count($tags)) == 0) {
-        echo '<p>No tags to display!</p>';
+        echo '<p>No tags to display!</p>'; 
     }
 
     echo '</ul>';
-
 } else {
     echo "<p>Error: " . htmlspecialchars($mysqli->error) . "</p>";
 }
+
+    
 
 ?>
